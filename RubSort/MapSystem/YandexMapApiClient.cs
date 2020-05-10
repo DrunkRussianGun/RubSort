04b7ab38fd2561b181155647ebc20a753ce0b707
Galimova Gulshat <gulshat-1999@mail.ru>
2020-05-10 23:47:06 +0300

    Добавила генерацию карты и получение точек приема

--- RubSort/MapSystem/YandexMapApiClient.cs
+++ RubSort/MapSystem/YandexMapApiClient.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
 using RubSort.Core;
 using RubSort.RecyclingPointsSystem;
 
@@ -23,7 +25,7 @@ namespace RubSort.MapSystem
         public Map GetMap(MapContext context)
         {
             var apiKey = settingsManager.GetSetting(yandexMapApiKeySetting);
-            var htmlScript = (string)null ?? throw new NotImplementedException(); // сгенерировать скрипт с картой
+            var htmlScript = GenerateMap(context, apiKey); // сгенерировать скрипт с картой
             return new Map { HtmlScript = htmlScript };
         }
 
@@ -32,12 +34,53 @@ namespace RubSort.MapSystem
             var apiKey = settingsManager.GetSetting(yandexMapApiKeySetting);
             var apiBaseUrl = settingsManager.GetSetting(yandexMapApiBaseUrlSetting);
             var apiOrganizationsUrl = settingsManager.GetSetting(yandexMapApiOrganizationsUrlSetting);
-            
+
             // с помощью httpClient отправить запрос по адресу apiBaseUrl + apiOrganizationsUrl
+            var response = httpClient.GetAsync(apiBaseUrl + apiOrganizationsUrl).Result;
+            var jsonString = response.Content.ReadAsStringAsync().Result;
             
             var recyclingPointInfo = new RecyclingPoint();
             // по полученному ответу заполнить recyclingPointInfo
+            recyclingPointInfo = JsonConvert.DeserializeObject<RecyclingPoint>(jsonString);
+            
             return recyclingPointInfo;
+        }
+
+        private string GenerateMap(MapContext context, string apiKey)
+        {
+            var builder = new StringBuilder();
+            builder.Append($@"<!DOCTYPE html>
+                <html xmlns=""http://www.w3.org/1999/xhtml"">
+                <head>
+                    <meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"" />
+                    <script src=""https://api-maps.yandex.ru/2.1/?apikey={apiKey}&lang=ru_RU"" type=""text/javascript"">
+                    </script>
+                    <script type=""text/javascript"">
+                        ymaps.ready(init);
+                        function init() {{
+                            var myMap = new ymaps.Map(""map"", {{ 
+                                center: [{context.InitialPoint.Latitude}, {context.InitialPoint.Longitude}],
+                                zoom: {context.Zoom} 
+                            }});");
+
+            var points = context.RecyclingPoints;
+            foreach (var point in points)
+            {
+                var newPoint = GetInfo(point);
+                builder.Append(
+                    $@"var myPlacemark = new ymaps.Placemark([{newPoint.GeoCoordinate.Latitude}, {newPoint.GeoCoordinate.Longitude}]);
+                    myMap.geoObjects.add(myPlacemark);");
+            }
+
+            builder.Append(@"}
+                    </script>
+                </head>
+
+                <body>
+                    <div id=""map"" style=""width: 1000px; height: 600px""></div>
+                </body>
+                </html>");
+            return builder.ToString();
         }
     }
 }
--- RubSort/RecyclingPointsSystem/RecyclingPointProvider.cs
+++ RubSort/RecyclingPointsSystem/RecyclingPointProvider.cs
@@ -1,21 +1,33 @@
 ﻿using System;
+using System.Linq;
 using RubSort.DataStorageSystem;
 
 namespace RubSort.RecyclingPointsSystem
 {
     public class RecyclingPointProvider
     {
-        private IEntityRepository<RecyclingPointDbo> entityRepository;
+        private readonly IEntityRepository<RecyclingPointDbo> _entityRepository;
 
         public RecyclingPointProvider(IEntityRepository<RecyclingPointDbo> entityRepository)
         {
-            this.entityRepository = entityRepository;
+            _entityRepository = entityRepository;
         }
         
         public RecyclingPoint[] GetRecyclingPoints()
         {
-            //todo
-            throw new NotImplementedException();
+            return _entityRepository.Get()
+                .Select(rp => new RecyclingPoint()
+                {
+                    Name = rp.Name,
+                    Description = rp.Description,
+                    Address = new Address()
+                    {
+                        AddressLine = rp.AddressAddressLine, City = rp.AddressCity, Country = rp.AddressCountry
+                    },
+                    Contacts = new Contacts() {Email = rp.ContactsEmail, Telephone = rp.ContactsTelephone},
+                    GeoCoordinate = rp.GeoCoordinate
+                })
+                .ToArray();
         }
     }
 }
